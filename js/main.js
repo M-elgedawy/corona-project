3364c44b8c090c9ab7a0cff23e39f072f4b15c80
M-elgedawy <aly100369@gmail.com>
2022-02-07 02:00:30 +0200

    modified

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ $(window).scroll (function(){
     
 
 })
+// making toggle button toggle collapse class to show nav in responsive width
+ const btn=document.querySelector('.navbar-toggler');
+ const collapseDiv=document.querySelector('.collapse')
+ const navLi=document.querySelectorAll('.nav-item');
+
+ btn.addEventListener('click',()=>{
+    collapseDiv.classList.toggle('collapse')
+ })
+ //when click on nav link hide the  collapse div.
+ navLi.forEach(function(li){
+     li.addEventListener('click',()=>{
+         collapseDiv.classList.add('collapse')
+     })
+ })
